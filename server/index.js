1380bfa1bfa8fb333ea60d41d37343337b402fef
Arafat Rahaman <96245505+Hieser21@users.noreply.github.com>
2023-07-27 21:57:11 +0600

    Update index.js

--- server/index.js
+++ server/index.js
@@ -28,45 +28,7 @@ app.route("/").get(function (req, res) {
 app.use(express.static('views'))
 app.post('/email',(req,response)=>{
     import('./verify.mjs').then(verify => {verify(req)})
-const output=`
-  <p>You have a new contact request</p>
-  <img class="email" src="cid:email" alt="email-image">
-  <h3>Contact details</h3>
-  <ul>
-  <li>Name: ${req.body.name}</li>
-  <li>Subject: ${req.body.subject}</li>
-  <li>Email: ${req.body.email}</li>
-  <li>Message: ${req.body.message}</li>
-  </ul>`
-const smtpTrans = nodemailer.createTransport({
-    host: 'smtp.gmail.com',
-    port: 465,
-    secure: true,
-  auth:{
-  type:"OAuth2",
-  user:process.env.GMAIL_USER,
-  clientId:process.env.Client_ID,
-  clientSecret:process.env.Client_Secret,
-  refreshToken:process.env.Refresh_Token,
-  accessToken:accessToken
-  }})
-const mailOpts = {
-  from:process.env.GMAIL_USER,
-  to:process.env.RECIPIENT,
-  subject:'Portfolio Website',
-  html:output
-}
-smtpTrans.sendMail(mailOpts,(error,res)=>{
-   if(error){
-   console.log(error);
-   }
-   else{
-    console.log("Message sent: " + res.message);
-    response.status(200).send('You can go back from this window now!')
-    }
-  //smtpTrans.close();
-   })
-})
+
 const port = process.env.PORT || 3000
 const server = app.listen(port,listening)
 function listening (){
