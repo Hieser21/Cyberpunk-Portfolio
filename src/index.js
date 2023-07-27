9076fb57890523d2983b81dcd0524ba43d40aaf7
Hieser21 <rarafat883@gmail.com>
2023-07-27 15:34:56 +0000

    Test version

--- src/index.js
+++ src/index.js
@@ -5,21 +5,39 @@ import { ThemeProvider, createTheme, Arwes, Image, Frame, Puffs, Words, Line, Li
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import './style.css';
-
+import {Animator} from '@arwes/react-animator'
+import { GridLines, Dots, MovingLines } from '@arwes/react-bgs';
+let siteKey = `${process.env.SITE_KEY}`
 const App = () => {
-  useEffect(() => {
-    toast.info('Welcome to Cyberpunk Portfolio', {
-      position: toast.POSITION.TOP_CENTER,
-      autoClose: 5000,
-      className: 'toast-custom-background',
-    });
-  }, []);
+ 
 
 return (
   <ThemeProvider theme={createTheme()}>
     <Arwes>
-      <ToastContainer />
-      <Puffs>
+    <Animator duration={{ interval: 10 }}>
+      <div style={{
+        position: 'absolute',
+        height: '160%',
+        left: 0,
+        right: 0,
+        top: 0,
+        bottom: 0,
+        backgroundColor: '#000906',
+        backgroundImage: 'radial-gradient(85% 85% at 50% 50%, hsla(185, 100%, 25%, 0.25) 0%, hsla(185, 100%, 25%, 0.12) 50%, hsla(185, 100%, 25%, 0) 100%)'
+      }}>
+        <GridLines
+          lineColor='hsla(180, 100%, 75%, 0.05)'
+          distance={50}
+        />
+        <Dots
+          color='hsla(180, 100%, 75%, 0.05)'
+          distance={50}
+        />
+        <MovingLines
+          lineColor='hsla(180, 100%, 75%, 0.07)'
+          distance={50}
+          sets={20}
+        />
         <div style={{padding: 20 }}>
           <Header animate>
             <h3 style={{ margin: 0,display: 'flex', alignItems: "center", justifyContent: "center", fontFamily:"Space Crusaders", fontSize: "25px"}}> &#8249;&#8249; Cyberpunk Portfolio &#8250;&#8250;</h3>
@@ -30,24 +48,42 @@ return (
             <div style={{ margin: '0 auto', padding: 20, maxWidth: 600 }}>
               <Line animate layer='disabled' />
               <div className='container'>
-                <Paragraph className='typed'><Words animate layer='success'>
-                  Hi, I'm Richard Brown and I'm a full stack developer.
-                </Words></Paragraph>
+                
+                  <h3 style={{textAlign: 'center'}}>xhax</h3>
+               
               </div>
               <Line animate layer='disabled' />
-              <Image animate resources='https://version1-breakpoint1.arwes.dev/static/img/wallpaper.jpg'>
-                The vast universe around us
-              </Image>
-              <Line animate layer='success' />
-              <div style={{ display: 'flex', justifyContent: 'space-between' }}>
-                <Button animator={{ animate: true }} palette='success' active><Link animate layer='success' href='https://github.com/rbrown29' target='_blank'>GitHub</Link></Button>
-                <Logo animate size={40} />
-                <Button animator={{ animate: true }} palette='success' active><Link animate layer='success' href='https://www.linkedin.com/' target='_blank'>LinkedIn</Link></Button>
+              <div style={{ display: 'flex', justifyContent: 'space-around' }}>
+                <Button animator={{ animate: true }} palette='success' active><Link animate layer='success' href='https://discord.com/users/988863332960321577' target='_blank'>Discord</Link></Button>
+                <Button animator={{ animate: true }} palette='success' active><Link animate layer='success' href='https://twitter.com/xhaxOfficial' target='_blank'>Twitter</Link></Button>
+                <Button animator={{ animate: true }} palette='success' active><Link animate layer='success' href='https://keybase.io/xhax' target='_blank'>Keybase</Link></Button>
               </div>
+              <div style={{ display: 'flex', justifyContent: 'space-around' }}>
+                <Button animator={{ animate: true }} palette='success' active><Link animate layer='success' href='https://en.pronouns.page/@xhax' target='_blank'>Pronouns.page</Link></Button>
+                <Button animator={{ animate: true }} palette='success' active><Link animate layer='success' href='https://cash.app/xhaxOfficial' target='_blank'>Cash App</Link></Button>
+              </div>
+              <div style={{ display: 'flex', justifyContent: 'space-around', marginBottom: '20px' }}>
+                <Button animator={{ animate: true }} palette='success' active><Link animate layer='success' href='https://github.com/xhaxOfficial' target='_blank'>GitHub</Link></Button>
+                <Button animator={{ animate: true }} palette='success' active><Link animate layer='success' href='https://reddit.com/user/xhaxOfficial' target='_blank'>Reddit</Link></Button>
+              </div>
+              <Line animate layer='success' />
+              <form className='node-email-form' method='post' action='/email' style={{colorScheme: 'dark'}}>
+                <h4 style={{textAlign: 'center', textDecoration: 'underline'}}>Contact Me!</h4>
+                <div className='input'><input type='text' placeholder='Name' name='name'></input></div>
+                <div className='input'><input type='text' placeholder='Email' name='email'></input></div>
+                <div className='input'><input type='text' placeholder='Message' name='message'></input></div>
+                <div class="cf-turnstile" data-sitekey={siteKey} data-theme="dark"></div>
+                <Button className='button' animator={{ animate: true }} palette='success' active>Submit</Button>
+              </form>
+              <Line animate layer='success' />
+              <h4 style={{textAlign: 'center', textDecoration: 'underline'}}>Legal</h4>
+              <Button animator={{animate: true}} style={{display: 'block', textAlign: 'center', paddingBottom: '8px'}} palette='success' active>Trademark Notice</Button>
+              <Button animator={{animate: true}} style={{display: 'block', textAlign: 'center'}} palette='success' active>Privacy Notice</Button>
             </div>
           </Frame>
         </div>
-      </Puffs>
+        </div>
+      </Animator>
     </Arwes>
   </ThemeProvider>
 );
